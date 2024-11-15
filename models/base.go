c36a0eae6922225f226bcf5169e740ff4b3c3be4
structix <janek@schoff.it>
2024-11-15 12:43:09 +0100

    Add base name and rearrange fields to reduce pointer size

--- models/base.go
+++ models/base.go
@@ -1,10 +1,11 @@
 package models
 
 type Base struct {
-	Position          Position `json:"position"`            // position of the base
-	Uid               uint32   `json:"uid"`                 // uid of the base
-	Player            uint32   `json:"player"`              // uid of the owning player
-	Population        uint32   `json:"population"`          // number of bits in the base
-	Level             uint32   `json:"level"`               // level of the base
-	UnitsUntilUpgrade uint32   `json:"units_until_upgrade"` // bits needed for until the next upgrade
+	Name              string   `json:"name"`
+	Position          Position `json:"position"`
+	Uid               uint32   `json:"uid"`
+	Player            uint32   `json:"player"`
+	Population        uint32   `json:"population"`
+	Level             uint32   `json:"level"`
+	UnitsUntilUpgrade uint32   `json:"units_until_upgrade"`
 }
